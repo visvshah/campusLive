e1d0b264939896ef8b8c4630a7f12218f8ee725a
VisvShah <visvshah1@gmail.com>
2022-09-17 12:41:31 -0400

    Map.jsx structure complete

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Map from './components/Map/Map';
 
 function App() {
   return (
     <div className="App">
+      <Map/>
     </div>
   );
 }
--- front/src/components/Map/Map.jsx
+++ front/src/components/Map/Map.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import './map.scss';
 
 export default function Map() {
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+    }
   return (
     <div>
-        Map
+        <div className="mapPage">
+            <div className="left">
+                <h1 className = "header">Create a Event!</h1>
+                <form autoComplete = "off" validate = "true" className = "form" onSubmit = {handleSubmit}>
+                    <input className = "input"></input>
+                    <input className = "input"></input>
+                    <input className = "input"></input>
+                </form>
+            </div>
+            <div className="right">
+                Hello
+            </div>
+        </div>
     </div>
   )
 }
