c318a6162d52d5eb1941157e1e66c480d66517b1
Jammy Wang <111794490+Jammy-Wang@users.noreply.github.com>
2022-09-17 19:44:39 -0400

    landing page final

--- front/src/components/Landing/Landing.jsx
+++ front/src/components/Landing/Landing.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './landing.css'
+import image from "./purdueCampus.png"
 
 export default function Landing() {
   return (
@@ -9,8 +10,24 @@ export default function Landing() {
         <div class="bg bg2"></div>
         <div class="bg bg3"></div>
         <div class="content">
-          <h1>Sliding Diagonals Background Effect</h1>
+          <h2> Telling American college students about events. </h2>
         </div>
+        <div class="content">
+          <h2> Event organizing made easy. </h2>
+        </div>
+        <div class="contentImage">
+          <h2> Want to know where and when a club meeting is?</h2>
+          <h2> We gotchu fam. </h2>
+          <img src={image} className = "image" alt="Purdue Campus Map"></img>
+        </div>
+        <div class="content">
+          <h2> Streamlining information about events to collegiate students across the U.S. </h2>
+        </div>
+        <div class="content">
+          <h2> "This website is very swag." - Jammy Wang </h2>
+        </div>
+
+        
       </div>
 
     </div>
