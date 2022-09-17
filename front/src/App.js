cbfed3d6d98c1a02d7aeea8bd66742c6f9b36f96
Jammy Wang <111794490+Jammy-Wang@users.noreply.github.com>
2022-09-17 13:12:44 -0400

    landing-page sync

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Landing from './components/Landing/Landing';
 
+import NavBar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Landing/>
+      <NavBar/>
     </div>
   );
 }
