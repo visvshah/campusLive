42dac67fa4a380e92dd2da87af78b6c1ad17aafa
VisvShah <visvshah1@gmail.com>
2022-09-17 21:25:53 -0400

    Map Implemented

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import Landing from './components/Landing/Landing';
 import NavBar from './components/Navbar/Navbar';
-
+import Map from './components/Map/Map';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-      <Landing/>
+      <Map/>
     </div>
   );
 }
--- front/src/components/Auth/Auth.jsx
+++ front/src/components/Auth/Auth.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
-
+import React from "react";
 export default function Auth() {
   return (
     <div>
-        Auth
+        
     </div>
   )
 }
